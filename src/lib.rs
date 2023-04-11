11a0b505fa48dcd1d6cfc4cbb6588b498f7ec6ef
SciDev5 <skhuber0@gmail.com>
2023-04-11 11:36:58 -0400

    add some test data

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+pub fn some_fn(a: i32) -> i32 {
+    a + 1
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::some_fn;
+
+    #[test]
+    fn test_some_fn() {
+        for i in -10 .. 10 {
+            assert_eq!(some_fn(i), i+1);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use mathlib::some_fn;
+
 fn main() {
+    println!("{}",some_fn(1));
     println!("\n\u{001b}[42m\u{001b}[30m :: hewwo world :: \u{001b}[0m\n");
 }
