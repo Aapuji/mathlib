5d4cb027082d5054e9f077af13ac33861aafbb3d
Aapuji <79770468+Aapuji@users.noreply.github.com>
2023-04-16 08:47:35 -0400

    Edited Add.rs - children -> operands

--- src/expr/add.rs
+++ src/expr/add.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Display, sync::Arc};
+use std::{fmt::Display, sync::Arc, collections::HashMap};
 use num_complex::Complex64;
 
 use crate::var::Var;
@@ -8,25 +8,25 @@ impl std::ops::Add for ArcExpr {
     type Output = ArcExpr;
 
     fn add(self, rhs: Self) -> Self::Output {
-        ArcExpr(Arc::new(ExprAdd::new(vec![self.0, rhs.0])))
+        ArcExpr(Arc::new(AddExpr::new(vec![self.0, rhs.0])))
     }
 }
 
 #[derive(Debug)]
-pub struct ExprAdd {
-    children: Vec<Arc<dyn Expr>>,
+pub struct AddExpr {
+    operands: Vec<Arc<dyn Expr>>,
 }
 
-impl ExprAdd {
-    pub fn new(children: Vec<Arc<dyn Expr>>) -> Self {
-        Self { children }
+impl AddExpr {
+    pub fn new(operands: Vec<Arc<dyn Expr>>) -> Self {
+        Self { operands }
     }
 }
 
-impl Display for ExprAdd {
+impl Display for AddExpr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "(")?;
-        for (i,arcexpr) in self.children[..].into_iter().enumerate() {
+        for (i, arcexpr) in self.operands[..].into_iter().enumerate() {
             if i > 0 {
                 write!(f, " + ")?;
             }
@@ -38,17 +38,17 @@ impl Display for ExprAdd {
     }
 }
 
-impl Expr for ExprAdd {
+impl Expr for AddExpr {
     fn is_variant_on(&self, var: Arc<Var>) -> bool {
-        self.children[..]
+        self.operands[..]
             .into_iter()
             .map(|v| v.is_variant_on(var.clone()))
             .reduce(|a, b| a || b)
             .unwrap_or(false)
     }
-    fn eval(&self, var_values: &std::collections::HashMap<&Var, num_complex::Complex64>) -> super::EvalResult {
+    fn eval(&self, var_values: &HashMap<&Var, Complex64>) -> super::EvalResult {
         let mut sum = Complex64::new(0.0, 0.0);
-        for term in &self.children {
+        for term in &self.operands {
             sum += term.eval(var_values)?
         }
         Ok(sum)
