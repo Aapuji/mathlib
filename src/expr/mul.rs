0177c58fd109ebfc88e55c46c26c38d2671fac86
Aapuji <79770468+Aapuji@users.noreply.github.com>
2023-04-16 09:02:21 -0400

    Update mul.rs - ExprMul -> MulExpr

--- src/expr/mul.rs
+++ src/expr/mul.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Display, sync::Arc};
+use std::{fmt::Display, sync::Arc, collections::HashMap};
 use num_complex::Complex64;
 
 use crate::var::Var;
@@ -8,25 +8,25 @@ impl std::ops::Mul for ArcExpr {
     type Output = ArcExpr;
 
     fn mul(self, rhs: Self) -> Self::Output {
-        ArcExpr(Arc::new(ExprMul::new(vec![self.0, rhs.0])))
+        ArcExpr(Arc::new(MulExpr::new(vec![self.0, rhs.0])))
     }
 }
 
 #[derive(Debug)]
-pub struct ExprMul {
-    children: Vec<Arc<dyn Expr>>,
+pub struct MulExpr {
+    operands: Vec<Arc<dyn Expr>>,
 }
 
-impl ExprMul {
-    pub fn new(children: Vec<Arc<dyn Expr>>) -> Self {
-        Self { children }
+impl MulExpr {
+    pub fn new(operands: Vec<Arc<dyn Expr>>) -> Self {
+        Self { operands }
     }
 }
 
-impl Display for ExprMul {
+impl Display for MulExpr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "(")?;
-        for (i,arcexpr) in self.children[..].into_iter().enumerate() {
+        for (i, arcexpr) in self.operands[..].into_iter().enumerate() {
             if i > 0 {
                 write!(f, " * ")?;
             }
@@ -38,19 +38,21 @@ impl Display for ExprMul {
     }
 }
 
-impl Expr for ExprMul {
+impl Expr for MulExpr {
     fn is_variant_on(&self, var: Arc<Var>) -> bool {
-        self.children[..]
+        self.operands[..]
             .into_iter()
             .map(|v| v.is_variant_on(var.clone()))
             .reduce(|a, b| a || b)
             .unwrap_or(false)
     }
-    fn eval(&self, var_values: &std::collections::HashMap<&Var, num_complex::Complex64>) -> super::EvalResult {
+        
+    fn eval(&self, var_values: &HashMap<&Var, Complex64>) -> super::EvalResult {
         let mut product = Complex64::new(1.0, 0.0);
         for term in &self.children {
             product *= term.eval(var_values)?
         }
+            
         Ok(product)
     }
 }
